8c035650bb086abc805050346bfa5ae8e9992a59
Mont464 <amontore@ucsc.edu>
2024-11-10 21:28:52 -0800

    Initial button for dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 // todo
+const APP_NAME = "Map Cashe";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+
+const button = document.createElement("button");
+button.innerHTML = "Button";
+app.append(button);
+
+button.onclick = () => {
+  alert("you clicked the button!");
+};
